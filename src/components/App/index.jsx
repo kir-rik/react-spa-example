6342b434b6fc7e0afdce0b2f672a616c1aaa8f0b
kir_rik <kir-rik-90@yandex.ru>
2020-09-10 18:46:53 +0400

    init

---
+++ src/components/App/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+export default class App extends React.PureComponent {
+  render() {
+
+
+    return (
+      <main>
+        <h1>Какой-то разный контент</h1>
+      </main>
+    );
+  }
+}
