38d6a4e5356cdd0049205bf6963fee225f9094e0
kir_rik <kir-rik-90@yandex.ru>
2020-09-11 18:19:58 +0400

    useeffect

--- src/components/Hooks/index.jsx
+++ src/components/Hooks/index.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [checked, setChecked] = useState(false);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    document.title = name;
+  }, [name]);
 
   return (
     <div>
-      <div>Поклацано {count} раз</div>
-      <button onClick={() => setCount(count + 1)}>Клац</button>
-      <br />
-      <br />
-      <br />
-      <div>Почекано: {checked ? 'Да ' : 'Нет'} <input type="checkbox" id="scales" name="scales" onChange={() => setChecked(!checked) } /></div>
+      <input placeholder="Имя" value={name} onChange={({ target: { value } }) => setName(value)} />
     </div>
   );
 };
