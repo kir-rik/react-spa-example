5329334843fa9d7a9ef411e44d5988526cd07cc0
kir_rik <kir-rik-90@yandex.ru>
2020-09-11 18:06:58 +0400

    useState

--- src/components/Hooks/index.jsx
+++ src/components/Hooks/index.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default ({ title }) => {
-  return <h1>{title}</h1>;
+  const [count, setCount] = useState(0);
+  const [checked, setChecked] = useState(false);
+
+  return (
+    <div>
+      <div>Поклацано {count} раз</div>
+      <button onClick={() => setCount(count + 1)}>Клац</button>
+      <br />
+      <br />
+      <br />
+      <div>Почекано: {checked ? 'Да ' : 'Нет'} <input type="checkbox" id="scales" name="scales" onChange={() => setChecked(!checked) } /></div>
+    </div>
+  );
 };
